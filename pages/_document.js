f18a5865877c14dc9b1081a66ed10bb387781e87
Gabriel Mania <mania.gabb@gmail.com>
2022-12-04 21:50:10 +0800

    revise _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,11 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function MyDocument() {
   return (
     <Html>
       <Head>
-        <script
-          src="https://kit.fontawesome.com/ed2f2fe40a.js"
-          crossOrigin="anonymous"
-        ></script>
+        <Script src="https://kit.fontawesome.com/ed2f2fe40a.js"></Script>
       </Head>
       <body>
         <Main />
