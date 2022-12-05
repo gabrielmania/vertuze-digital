e406bd079497cfe3ba6456637c72ff99204de0ba
Gabriel Mania <mania.gabb@gmail.com>
2022-12-05 11:13:29 +0800

    add loading screen

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,29 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 import "../styles/globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import { useRouter } from "next/router";
+
 import { useUser } from "../lib/hooks";
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
   const user = useUser();
+  const [pageLoading, setPageLoading] = useState(false);
+
+  useEffect(() => {
+    const handleStart = () => {
+      setPageLoading(true);
+    };
+    const handleComplete = () => {
+      setPageLoading(false);
+    };
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+  }, [router]);
 
   return (
     <>
@@ -19,9 +35,18 @@ function MyApp({ Component, pageProps }) {
         />
         <link rel="icon" href="/favicon.png" />
       </Head>
-      {router.pathname !== "/" && <Navbar user={user} />}
-      <Component {...pageProps} user={user} />
-      {router.pathname !== "/" && <Footer />}
+      {pageLoading ? (
+        <div className="h-screen flex flex-col justify-center items-center">
+          <img src="/logos/light-nobg.png" className="animate-bounce w-3/12" />
+          <h5 className="text-2xl font-bold mt-5">Loading content. Please wait...</h5>
+        </div>
+      ) : (
+        <>
+          {router.pathname !== "/" && <Navbar user={user} />}
+          <Component {...pageProps} user={user} />
+          {router.pathname !== "/" && <Footer />}
+        </>
+      )}
     </>
   );
 }
